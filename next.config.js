5745aefa4a9b810c59c8fbc5a6a7266345153558
mt20careercoach <internal@mt20career.org>
2026-01-18 13:43:06 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-}
+  images: { unoptimized: true },
+  basePath: '/mt2.0-queuing-system',
+  assetPrefix: '/mt2.0-queuing-system/',
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
